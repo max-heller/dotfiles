6ba55238dd60ddc9d6cda5a66a54e605b9542eae
mxheller <heller.max1@gmail.com>
2018-12-17 17:26:47 -0500

    Updated linking script

--- link.sh
+++ link.sh
@@ -1,8 +1,11 @@
-ln -s -f `pwd`/"Code - OSS"/settings.json ~/.config/"Code - OSS"/User
-ln -s -f `pwd`/dunst/dunstrc ~/.config/dunst
-ln -s -f `pwd`/i3/config ~/.config/i3
-ln -s -f `pwd`/i3status/config ~/.config/i3status
-ln -s -f `pwd`/rofi/config ~/.config/rofi
-ln -s -f `pwd`/rofi/solarized.rasi ~/.config/rofi
-ln -s -f `pwd`/ssh/config ~/.ssh
-ln -s -f `pwd`/termite/config ~/.config/termite
+dotfiles=$(dirname "$(realpath $0)")
+
+ln -s -f $dotfiles/"Code - OSS"/settings.json ~/.config/"Code - OSS"/User
+ln -s -f $dotfiles/dunst/dunstrc ~/.config/dunst
+ln -s -f $dotfiles/i3/config ~/.config/i3
+ln -s -f $dotfiles/i3status/config ~/.config/i3status
+ln -s -f $dotfiles/rofi/config ~/.config/rofi
+ln -s -f $dotfiles/rofi/solarized.rasi ~/.config/rofi
+ln -s -f $dotfiles/ssh/config ~/.ssh
+ln -s -f $dotfiles/termite/config ~/.config/termite
+ln -s -f $dotfiles/.zshrc ~
