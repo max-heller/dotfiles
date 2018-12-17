71e763c8d527ced919586ba61c15da271bfdfeb3
mxheller <heller.max1@gmail.com>
2018-12-17 17:17:03 -0500

    Reorganization, linking script

---
+++ link.sh
@@ -0,0 +1,8 @@
+ln -s -f `pwd`/"Code - OSS"/settings.json ~/.config/"Code - OSS"/User
+ln -s -f `pwd`/dunst/dunstrc ~/.config/dunst
+ln -s -f `pwd`/i3/config ~/.config/i3
+ln -s -f `pwd`/i3status/config ~/.config/i3status
+ln -s -f `pwd`/rofi/config ~/.config/rofi
+ln -s -f `pwd`/rofi/solarized.rasi ~/.config/rofi
+ln -s -f `pwd`/ssh/config ~/.ssh
+ln -s -f `pwd`/termite/config ~/.config/termite
