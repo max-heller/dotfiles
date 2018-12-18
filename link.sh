4dd256c694ebc727a96433d024dcae7c5b3ad2ee
mxheller <heller.max1@gmail.com>
2018-12-18 01:09:05 -0500

    git and vim configs

--- link.sh
+++ link.sh
@@ -3,10 +3,12 @@ dotfiles=$(dirname "$(realpath $0)")
 ln -s -f $dotfiles/"Code - OSS"/settings.json ~/.config/"Code - OSS"/User
 ln -s -f $dotfiles/compton/compton.conf ~/.config
 ln -s -f $dotfiles/dunst/dunstrc ~/.config/dunst
+ln -s -f $dotfiles/git/.gitconfig ~
 ln -s -f $dotfiles/i3/config ~/.config/i3
 ln -s -f $dotfiles/i3status/config ~/.config/i3status
 ln -s -f $dotfiles/rofi/config ~/.config/rofi
 ln -s -f $dotfiles/rofi/solarized.rasi ~/.config/rofi
 ln -s -f $dotfiles/ssh/config ~/.ssh
 ln -s -f $dotfiles/termite/config ~/.config/termite
+ln -s -f $dotfiles/vim/.vimrc ~
 ln -s -f $dotfiles/zsh/.zshrc ~
---
+++ vim/.vimrc
@@ -0,0 +1,5 @@
+set viminfo='20,<1000,s1000
+set tabstop=4
+set softtabstop=4
+set expandtab
+syntax on
