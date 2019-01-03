36bfee1f02309fbe31d0ad3d5c71289893b455ba
mxheller <heller.max1@gmail.com>
2019-01-03 12:47:49 -0500

    linker cleanup

--- link.sh
+++ link.sh
@@ -2,11 +2,15 @@ dotfiles=$(dirname "$(realpath $0)")
 
 ln -s -f $dotfiles/"Code - OSS"/settings.json ~/.config/"Code - OSS"/User
 ln -s -f $dotfiles/compton/compton.conf ~/.config
-mkdir ~/.config/dunst
-ln -s -f $dotfiles/dunst/dunstrc ~/.config/dunst
+rm -r ~/.config/dunst
+ln -s -f $dotfiles/dunst ~/.config
 ln -s -f $dotfiles/git/.gitconfig ~
+rm -r ~/.config/i3
+ln -s -f $dotfiles/i3 ~/.config
 mkdir ~/.config/polybar
 ln -s -f $dotfiles/polybar/config ~/.config/polybar
+rm -r ~/.config/rofi
+ln -s -f $dotfiles/rofi ~/.config
 mkdir ~/.ssh
 ln -s -f $dotfiles/ssh/config ~/.ssh
 mkdir ~/.config/termite
