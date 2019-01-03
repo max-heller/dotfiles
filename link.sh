853cad88b78a20eb6ff8374ad67025866de934c0
mxheller <heller.max1@gmail.com>
2019-01-03 12:47:13 -0500

    Switch from i3status to polybar and nerd-fonts to font-awesome

--- link.sh
+++ link.sh
@@ -5,11 +5,8 @@ ln -s -f $dotfiles/compton/compton.conf ~/.config
 mkdir ~/.config/dunst
 ln -s -f $dotfiles/dunst/dunstrc ~/.config/dunst
 ln -s -f $dotfiles/git/.gitconfig ~
-ln -s -f $dotfiles/i3/config ~/.config/i3
-ln -s -f $dotfiles/i3status/config ~/.config/i3status
-mkdir ~/.config/rofi
-ln -s -f $dotfiles/rofi/config ~/.config/rofi
-ln -s -f $dotfiles/rofi/solarized.rasi ~/.config/rofi
+mkdir ~/.config/polybar
+ln -s -f $dotfiles/polybar/config ~/.config/polybar
 mkdir ~/.ssh
 ln -s -f $dotfiles/ssh/config ~/.ssh
 mkdir ~/.config/termite
