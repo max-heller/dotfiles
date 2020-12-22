96cc2bee66f21bf776f7a1813e83c187680a10e6
mxheller <max.a.heller@gmail.com>
2020-12-22 10:28:01 -0500

    remove spotify-tui

--- post-install.sh
+++ post-install.sh
@@ -2,7 +2,7 @@ dotfiles=$(dirname "$(realpath $0)")
 
 # Packages
 yay
-yay -S --needed --norebuild bibata-cursor-theme discord dunst feh fzf gdb google-chrome gtk-theme-numix-solarized insync jre-openjdk kitty lightdm lightdm-slick-greeter lightdm-settings lxappearance lxqt-policykit mons npm neovim otf-fira-mono-italic-git oh-my-zsh-git playerctl polybar python python-pip redshift rofi rofi-calc rofi-dmenu rustup slack-desktop spotify-tui texlive-most the_silver_surfer thunar tree tslint ttf-fira-code ttf-font-awesome typescript zathura zathura-pdf-poppler zoom zsh zsh-syntax-highlighting
+yay -S --needed --norebuild bibata-cursor-theme discord dunst feh fzf gdb google-chrome gtk-theme-numix-solarized insync jre-openjdk kitty lightdm lightdm-slick-greeter lightdm-settings lxappearance lxqt-policykit mons npm neovim otf-fira-mono-italic-git oh-my-zsh-git playerctl polybar python python-pip python-pylint redshift rofi rofi-calc rofi-dmenu ripgrep rustup slack-desktop texlive-most the_silver_surfer thunar tree tslint ttf-fira-code ttf-font-awesome typescript unzip zathura zathura-pdf-poppler zip zoom zsh zsh-syntax-highlighting
 
 # Link dotfiles
 $dotfiles/link.sh
