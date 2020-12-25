dd6675afb4aa3a7efe46628541ab453cdd5bc759
max-heller <max.a.heller@gmail.com>
2020-12-25 09:47:34 -0500

    combine post-install.sh and link.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 dotfiles=$(dirname "$(realpath $0)")
 
+# Packages
+yay
+yay -S --needed --norebuild - < packages.txt
+
+# Link dotfiles
 ln -s -f $dotfiles/.config ~
 ln -s -f $dotfiles/.fehbg ~
 ln -s -f $dotfiles/.gitconfig ~
 mkdir -p ~/.ssh && ln -s -f $dotfiles/ssh/config ~/.ssh
 ln -s -f $dotfiles/.zshrc ~
+
+# Set ZSH as shell
+chsh -s /usr/bin/zsh
+
+# Enable services
+systemctl --user enable redshift
