ad2f6b84d8df824f48d1a6df3bf2b6a083fad59c
max-heller <max.a.heller@gmail.com>
2020-12-24 22:22:42 -0500

    embed .config in dotfiles

--- link.sh
+++ link.sh
@@ -1,21 +1,6 @@
 dotfiles=$(dirname "$(realpath $0)")
 
-ln -s -f $dotfiles/alacritty ~/.config
-rm -r ~/.config/dunst
-ln -s -f $dotfiles/dunst ~/.config
-ln -s -f $dotfiles/git/.gitconfig ~
-rm -r ~/.config/i3
-ln -s -f $dotfiles/i3 ~/.config
-ln -s -f $dotfiles/nvim ~/.config
-rm -rf ~/.config/polybar
-mkdir ~/.config/polybar
-ln -s -f $dotfiles/polybar/config ~/.config/polybar
-mkdir -p ~/.config/redshift
-ln -s -f $dotfiles/redshift/redshift.conf ~/.config/redshift
-rm -r ~/.config/rofi
-ln -s -f $dotfiles/rofi ~/.config
-mkdir -p ~/.ssh
-ln -s -f $dotfiles/ssh/config ~/.ssh
-ln -s -f $dotfiles/starship.toml ~/.config
-ln -s -f $dotfiles/zathura ~/.config
-ln -s -f $dotfiles/zsh/.zshrc ~
+ln -s -f $dotfiles/.config ~
+ln -s -f $dotfiles/.gitconfig ~
+mkdir -p ~/.ssh && ln -s -f $dotfiles/ssh/config ~/.ssh
+ln -s -f $dotfiles/.zshrc ~
