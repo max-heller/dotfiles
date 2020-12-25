f77e85337f0aba5239e19e6bd23233ab802c2754
max-heller <max.a.heller@gmail.com>
2020-12-25 09:22:10 -0500

    feh

--- link.sh
+++ link.sh
@@ -1,6 +1,7 @@
 dotfiles=$(dirname "$(realpath $0)")
 
 ln -s -f $dotfiles/.config ~
+ln -s -f $dotfiles/.fehbg ~
 ln -s -f $dotfiles/.gitconfig ~
 mkdir -p ~/.ssh && ln -s -f $dotfiles/ssh/config ~/.ssh
 ln -s -f $dotfiles/.zshrc ~
